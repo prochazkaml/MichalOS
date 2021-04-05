e410abac160df42981864c5068076a1f840f572b
Michal Procházka <prochazka2003@gmail.com>
2021-04-05 10:40:33 +0200

    Basic arcade collection program added

---
+++ programs/arcade.asm
@@ -0,0 +1,32 @@
+; ------------------------------------------------------------------
+; MichalOS Arcade
+; ------------------------------------------------------------------
+
+	%INCLUDE "michalos.inc"
+
+start:
+	mov ax, .gamelist
+	mov bx, .help_msg1
+	mov cx, .help_msg2
+	mov si, .callback
+	call os_list_dialog_tooltip
+
+	shl ax, 1
+	mov bx, ax
+
+	mov byte [0082h], 1
+	call [.bootlist - 2 + bx]
+
+.dummy:
+	ret
+
+.callback:
+	ret
+
+	.gamelist	db "SorryAss,Exit", 0
+	.help_msg1	db "Please select a game:"	; No need to zero-terminate here
+	.help_msg2	db 0
+
+	.bootlist	dw sorryass, .dummy
+
+	%include "arcade/sorryass.asm"
