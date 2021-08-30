9e05d31a74f11163a665b4351ab165623220f97d
Michal Procházka <prochazka2003@gmail.com>
2021-08-30 11:58:44 +0200

    Added a simple dead pixel tester

---
+++ programs/deadpixl.asm
@@ -0,0 +1,38 @@
+; ------------------------------------------------------------------
+; MichalOS Dead Pixel Test
+; ------------------------------------------------------------------
+
+	%INCLUDE "michalos.inc"
+
+start:
+	mov ax, .msg1
+	mov bx, .msg2
+	mov cx, 0
+	mov dx, 0
+	call os_dialog_box
+
+	mov byte [0082h], 1
+
+	mov ax, 13h
+	int 10h
+	
+	clr bx
+
+.loop:
+	call os_clear_graphics
+
+	inc bl
+	and bl, 7
+
+	call os_wait_for_key
+	cmp al, 27
+	jne .loop
+		
+	mov ax, 3
+	int 10h
+	ret
+	
+	.msg1	db "Press any key to change the color.", 0
+	.msg2	db "Press Escape to quit.", 0
+
+; ------------------------------------------------------------------
