a8d1eaf545c989a9d93b35d4a7ada4614b594520
Michal Procházka <michal@prochazka.ml>
2022-10-20 20:57:08 +0200

    kerneltree.py now shows which function belongs to which source file

--- kerneltree.py
+++ kerneltree.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 import re
+from xml.etree.ElementInclude import include
 
 table = open("build/kernel.lst")
 rows = table.read().replace("\t", " ").splitlines()
 
 oldname = ""
 newname = ""
+oldinclude = ""
+newinclude = ""
 oldstartaddr = 0
+includefound = False
 firstaddrfound = False
 
 for row in rows:
 	el = list(filter(None, row.split(' ')))
+
+	includefound = False
 
 	for e in el:
 		if ";" in e:
@@ -24,13 +30,20 @@ for row in rows:
 			newname = match[0]
 			firstaddrfound = False
 
+		if "%INCLUDE" in e:
+			includefound = True
+
+		if includefound:
+			newinclude = e
+
 	try:
 		addr = int(el[1], base=16)
 
 		if not firstaddrfound:
 			if oldname != "":
-				print("%d %s" % (addr - oldstartaddr, oldname))
+				print("%d %s %s" % (addr - oldstartaddr, oldname, oldinclude))
 
+			oldinclude = newinclude
 			oldstartaddr = addr
 			firstaddrfound = True
 
