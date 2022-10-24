0e0fe07dfedcfde5c43eb616eca62b5c3eb5bb96
Michal Procházka <michal@prochazka.ml>
2022-10-24 15:40:13 +0200

    Added the 3D starfield demo to the main menu

--- kernel/main.asm
+++ kernel/main.asm
@@ -708,7 +708,7 @@ systemfilemissing:
 	noprogerror				db 'System file not found', 0
 
 	appindex1				dw edit_name, viewer_name, calc_name, clock_name, cmd_name, config_name, ascii_name, pixel_name, player_name, hwcheck_name, about_name
-	debugindex1				dw debug0_name, debug1_name, debug2_name, debug3_name, debug4_name, debug5_name, debug6_name, debug7_name, debug8_name, debug9_name, debug11_name, debug12_name, debug13_name
+	debugindex1				dw debug0_name, debug1_name, debug2_name, debug3_name, debug4_name, debug5_name, debug6_name, debug7_name, debug8_name, debug9_name, debug10_name, debug11_name, debug12_name, debug13_name
 	gameindex1				dw cf_name, inkspill_name, spaceinv_name, asmtris_name, sudoku_name, fisher_name, miketron_name, muncher_name, hangman_name, snake_name
 	
 	edit_name				db 'EDIT', 0
@@ -733,6 +733,7 @@ systemfilemissing:
 	debug7_name				db 'SHAPES', 0
 	debug8_name				db 'DOTS', 0
 	debug9_name				db 'DEADPIXL', 0
+	debug10_name			db 'STARS', 0
 	debug11_name			db 'CHECK', 0
 	debug12_name			db 'RDTSC', 0
 	debug13_name			db 'TEST', 0
@@ -764,7 +765,7 @@ systemfilemissing:
 	
 	menuoptions				db 'Programs,Games,Log out,Reboot,Power off', 0
 	gameoptions				db 'Cosmic Flight,InkSpill,Space Inventors,aSMtris,Sudoku,Deep Sea Fisher,MikeTron,Muncher,Hangman,Snake', 0
-	debugoptions			db 'Font editor,Disk detection test,Keyboard tester,Serial communication tester,RTC clock tester,Disk Sector inspector,Memory editor,Shapes test,Dots,Dead pixel tester,Disk sector checker,TSC register tester,Simple test app', 0
+	debugoptions			db 'Font editor,Disk detection test,Keyboard tester,Serial communication tester,RTC clock tester,Disk Sector inspector,Memory editor,Shapes test,Dots,Dead pixel tester,3D Starfield demo,Disk sector checker,TSC register tester,Simple test app', 0
 	progoptions				db 'File manager,Text editor,Image viewer,Calculator,Clock,Terminal,Settings,ASCII art editor,Pixel art editor,Music player,Hardware checker,About MichalOS,Other stuff...', 0
 	
 ; ------------------------------------------------------------------
