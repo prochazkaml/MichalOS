prochazkaml/MichalOS

commit d5f3dd4a57f137d97f33bbf8b83c0cc48694eab8

Screensaver now works when the timer is set to other frequencies

kernel/features/int.asm
@@ -89,7 +89,7 @@ os_return_app_timer:
 ; OUT: Nothing, registers preserved
 
 os_set_timer_speed:
-	pusha
+	pushad
 	
 .no_pusha:
 	mov [current_timer_speed], cx
@@ -100,8 +100,19 @@ os_set_timer_speed:
 	out 40h, al
 	mov al, ch
 	out 40h, al
-	
-	popa
+
+	clr edx
+	mov eax, 105000000*60/88	; Ticks per minute
+
+	dec cx				; 0x0000 -> 0xFFFF
+	movzx ecx, cx
+	inc ecx				; 0xFFFF -> 0x10000
+
+	div ecx
+
+	mov [current_timer_freq], eax
+
+	popad
 	ret
 	
 ; -----------------------------------------------------------------
@@ -155,10 +166,10 @@ os_compat_int1C:
 	mov ds, ax
 	mov es, ax
 	
-	cmp word [screensaver_timer], 0
+	cmp dword [screensaver_timer], 0
 	je .no_update_screensaver
 	
-	dec word [screensaver_timer]
+	dec dword [screensaver_timer]
 	
 .no_update_screensaver:
 	cmp word [pause_timer], 0
@@ -202,8 +213,9 @@ os_compat_int1C:
 	timer_application_offset	dw 0
 	
 	current_timer_speed			dw 0
-	
-	screensaver_timer			dw 0
+	current_timer_freq			dd 0	; in Hz/64
+
+	screensaver_timer			dd 0
 	pause_timer					dw 0
 
 ; ==================================================================

kernel/features/keyboard.asm
@@ -16,10 +16,10 @@ os_wait_for_key:
 	call .screen_power
 
 	; Reset the screensaver tick
-	movzx ax, byte [57074]
-	mov bx, 1092		; 18.2 Hz * 60 seconds
-	mul bx
-	mov [screensaver_timer], ax		; See "int.asm"
+	movzx eax, byte [57074]
+	mov ebx, [current_timer_freq]	; Multiply by the number of ticks per minute
+	mul ebx
+	mov [screensaver_timer], eax	; See "int.asm"
 	
 	mov byte [.scrn_active], 0	; Reset all the screensaver variables
 
@@ -59,7 +59,7 @@ os_wait_for_key:
 	ret
 	
 .screensaver:
-	cmp word [screensaver_timer], 0
+	cmp dword [screensaver_timer], 0
 	jne .good
 	
 	cmp byte [57074], 0

kernel/main.asm
@@ -281,20 +281,12 @@ os_main:
 	mov si, os_compat_int07
 	call os_modify_int_handler
 
-	mov cl, 1Ch					; RTC handler
-	mov si, os_compat_int1C
-	call os_modify_int_handler
-	
 	sti
-
-;	int 5
 	
-	mov di, 100h
-	clr al
-	mov cx, 7EFFh
-	rep stosb
-
 	call os_init_text_mode
+	call os_return_app_timer	; Also sets up RTC handler
+	clr cx
+	call os_set_timer_speed
 	
 	mov ax, 0305h
 	mov bx, 0104h

commit 5c5e3171c984ae283e3f76d60682027ea82b1d55

Fixed bug introduced by d5f3dd4

kernel/features/int.asm
@@ -89,7 +89,7 @@ os_return_app_timer:
 ; OUT: Nothing, registers preserved
 
 os_set_timer_speed:
-	pushad
+	pusha
 	
 .no_pusha:
 	mov [current_timer_speed], cx
@@ -101,6 +101,7 @@ os_set_timer_speed:
 	mov al, ch
 	out 40h, al
 
+	pushad
 	clr edx
 	mov eax, 105000000*60/88	; Ticks per minute
 
@@ -111,8 +112,9 @@ os_set_timer_speed:
 	div ecx
 
 	mov [current_timer_freq], eax
-
 	popad
+
+	popa
 	ret
 	
 ; -----------------------------------------------------------------